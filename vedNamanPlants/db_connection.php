98512975b865c60451a581934459073a322e482f
arpitramoliya <155828563+arpitramoliya@users.noreply.github.com>
2024-01-20 19:09:25 +0530

    Delete vedNamanPlants directory

--- vedNamanPlants/db_connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-// SQLite database file
-$databaseFile = 'plantsData.sqlite';
-
-// Create or open the SQLite database
-$conn = new PDO('sqlite:' . $databaseFile);
-
-// Set the PDO error mode to exception
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
